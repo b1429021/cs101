f4a24cbf2441abf67031ab30ad94aff5aeee06f2
b1429021 <lulala0627@gmail.com>
2025-10-02 17:49:47 +0800

    Update main3.c

--- EX3/main3.c
+++ EX3/main3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() {
     int i = 51;
-    if(i % 15 == 0) {
+    if (i % 15 == 0) {
         printf("LOVE IU");
     }
-    else if(i % 5 == 0) {
+    else if (i % 5 == 0) {
         printf("IU");
     }
     else if (i % 3 == 0) {
